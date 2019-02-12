6b3dbc3edec715c33cbdea1d93c485ad86f265f1
Mohamed Amn <mamn2@illinois.edu>
2019-02-12 11:17:06 -0600

    Separated main game engine, game now prints all messages to console, test cases passed

---
+++ src/com/example/MainGameEngine.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class MainGameEngine {
+
+    public static void main(String[] args) {
+        Crazy8Game crazy8Game = new Crazy8Game(6);
+        crazy8Game.playGame();
+    }
+
+}
